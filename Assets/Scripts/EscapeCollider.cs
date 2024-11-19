6eb180b6deb187c050ee615af1a4dfe29260ba36
tu <bhtu.work>
2024-11-19 20:03:28 +0000

    add escape collider

---
+++ Assets/Scripts/EscapeCollider.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeCollider : MonoBehaviour
+{
+    // check if collide with player
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Collide with " + other.tag);
+
+        if (other.tag == "Player")
+        {
+            // Win 
+        }
+    }
+}
