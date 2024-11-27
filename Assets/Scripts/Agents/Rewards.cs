87d1f9b3a3f1ce2bbf372816c0fb6200771fd95c
tu <bhtu.work>
2024-11-27 17:32:06 +0000

    rewards definition

---
+++ Assets/Scripts/Agents/Rewards.cs
@@ -0,0 +1,16 @@
+public static class Rewards
+{
+    ////// Common rewards
+    public static float WIN = 100f;
+    public static float LOSE = -100f;
+    public static float HIT_WALL = -0.01f;
+
+    ////// Finder rewards
+    public static float FOUND_ESCAPER = 0.1f;
+    public static float CHASING_ESCAPER = 0.01f;
+
+    ////// Escaper rewards
+    public static float FOUND_EXIT = 0.1f;
+    public static float TO_EXIT = 0.01f;
+
+}
