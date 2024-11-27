f476d316a8c68686d0c23e2efc55121e11eb1301
tu <bhtu.work>
2024-11-27 14:34:11 +0000

    improve on movement type during heuristic

--- Assets/Scripts/EscaperAgent.cs
+++ Assets/Scripts/EscaperAgent.cs
@@ -11,6 +11,11 @@ public class EscaperAgent : Agent
     public static EscaperAgent INSTANCE;
 
 
+    [Header("Movement Settings")]
+    [Tooltip("The type of movement to use for the agent. Infers from the CharacterMovement component if not set.")]
+    public KeyMoveType heuristicsMoveType = KeyMoveType.NONE;
+
+
     private CharacterMovement characterMovement;
     private MovementInput heuristicsMove;
 
@@ -40,16 +45,13 @@ public class EscaperAgent : Agent
 
     public override void OnEpisodeBegin()
     {
-        this.heuristicsMove = new MovementInput
-        {
-            keyMoveType = this.characterMovement.keyMoveType
-        };
+        // Use movement if not set
+        if (this.heuristicsMoveType == KeyMoveType.NONE) {
+            this.heuristicsMoveType = this.characterMovement.keyMoveType;
+        }
 
         // Stop external inputs if in training mode
-        if (Academy.Instance.IsCommunicatorOn)
-        {
-            this.characterMovement.keyMoveType = KeyMoveType.NONE;
-        }
+        this.characterMovement.keyMoveType = KeyMoveType.NONE;
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -74,6 +76,9 @@ public class EscaperAgent : Agent
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
+        // check for changes on the fly
+        this.heuristicsMove.keyMoveType = this.heuristicsMoveType;
+
         var actions = actionsOut.DiscreteActions;
         actions[0] = this.heuristicsMove.GetBurstInput() ? 1 : 0;
         actions[1] = (int)this.heuristicsMove.GetMovementInput();
